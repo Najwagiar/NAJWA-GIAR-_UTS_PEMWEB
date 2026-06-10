89a989ca2b424b15de24f5d017fb464de5bd1aad
awa <najwagiar@gmail.com>
2026-06-10 17:45:40 +0700

    FIKS BGT

--- src/pages/dashboard/DashboardIndex.tsx
+++ src/pages/dashboard/DashboardIndex.tsx
@@ -7,7 +7,7 @@ export default function DashboardIndex() {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fetch("https://crudnajwagiarekaazzahra-production.up.railway.app/categories/")
+    fetch("http://localhost:3000/category/")
       .then((res) => res.json())
       .then((data) => setCategories(data.data || data))
       .catch(() => {});
